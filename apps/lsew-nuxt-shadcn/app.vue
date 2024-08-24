50c84a664c8c40b7ee84cae21d6923aab479c6a7
Patrick Ineichen <github@frokonet.ch>
2024-08-24 17:04:14 +0200

    feat(lsew-nuxt-shadcn): add hero page

--- apps/lsew-nuxt-shadcn/app.vue
+++ apps/lsew-nuxt-shadcn/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <h1>LSEW - Luzerner Solo- und Ensemblewettbewerb</h1>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ apps/lsew-nuxt-shadcn/components/hero-section.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const nextCompetitionDate = '28./29. September 2024';
+const nextCompetitionLocation = 'Marbach LU';
+</script>
+
+<template>
+  <main class="mx-4 flex flex-col items-center lg:mx-auto lg:w-1/2">
+    <h1 class="my-8 text-center text-4xl font-bold md:text-6xl">
+      Luzerner <span class="text-5xl text-secondary md:text-8xl">Solo</span> und
+      <span class="text-5xl text-secondary md:text-8xl">Ensemble</span> Wettbewerb
+    </h1>
+
+    <img src="assets/img/scores.png" alt="scores" >
+
+    <div class="mt-8 rounded-xl border-2 border-secondary bg-accent p-8 text-center">
+      <p>Nächster Wettbewerb:</p>
+      <p class="font-bold">{{ nextCompetitionDate }}</p>
+      <p class="text-2xl font-bold text-secondary">in {{ nextCompetitionLocation }}</p>
+      <Button class="mt-8">Jetzt anmelden</Button>
+    </div>
+  </main>
+</template>
