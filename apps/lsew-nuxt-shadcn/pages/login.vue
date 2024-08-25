e6fc73f20be28e67e61eb28e33a2dfe24cb4553e
Patrick Ineichen <github@frokonet.ch>
2024-08-25 18:07:22 +0200

    feat(lsew-nuxt-shadcn): add login form

--- apps/lsew-nuxt-shadcn/pages/login.vue
+++ apps/lsew-nuxt-shadcn/pages/login.vue
@@ -1,5 +1,47 @@
+<script setup lang="ts">
+import { useForm } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/zod';
+import * as z from 'zod';
+
+const formSchema = toTypedSchema(
+  z.object({
+    username: z.string(),
+    password: z.string()
+  })
+);
+
+const { handleSubmit } = useForm({
+  validationSchema: formSchema
+});
+
+const onSubmit = handleSubmit((values) => {
+  console.log(values);
+});
+</script>
+
 <template>
   <article class="prose m-4 mt-8 dark:prose-invert lg:prose-xl lg:mx-auto">
     <h1 class="text-secondary">Login</h1>
+    <form class="w-2/3 space-y-6" @submit="onSubmit">
+      <FormField v-slot="{ componentField }" name="username">
+        <FormItem>
+          <FormLabel>Username</FormLabel>
+          <FormControl>
+            <Input type="text" v-bind="componentField" />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+      <FormField v-slot="{ componentField }" name="password">
+        <FormItem>
+          <FormLabel>Password</FormLabel>
+          <FormControl>
+            <Input type="password" v-bind="componentField" />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+      <Button type="submit">Submit</Button>
+    </form>
   </article>
 </template>
