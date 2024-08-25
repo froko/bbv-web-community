b2841a826e42a8c56ca1ba24ca6028a234144a53
Patrick Ineichen <github@frokonet.ch>
2024-08-25 20:07:21 +0200

    feat(lsew-nuxt-shadcn): add seo data

--- apps/lsew-nuxt-shadcn/nuxt.config.ts
+++ apps/lsew-nuxt-shadcn/nuxt.config.ts
@@ -5,5 +5,16 @@ export default defineNuxtConfig({
   modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode', 'shadcn-nuxt'],
   colorMode: {
     classSuffix: ''
+  },
+  app: {
+    head: {
+      title: 'Luzerner Solo und Ensemble Wettbewerb',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        { name: 'description', content: 'Luzerner Solo und Ensemble Wettbewerb' },
+        { name: 'author', content: 'bbv Software Services AG' }
+      ]
+    }
   }
 });
