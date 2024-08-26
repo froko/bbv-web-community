649451bd2d19d74d0a1d619a6c9b098d8e77eb5f
Patrick Ineichen <github@frokonet.ch>
2024-08-26 08:30:16 +0200

    fix(lsew-nuxt-shadcn): add mandatory anmeldung fields as plain text

--- apps/lsew-nuxt-shadcn/pages/anmeldung.vue
+++ apps/lsew-nuxt-shadcn/pages/anmeldung.vue
@@ -1,5 +1,15 @@
 <template>
   <article class="prose m-4 mt-8 dark:prose-invert lg:prose-xl lg:mx-auto">
     <h1 class="text-secondary">Anmeldung</h1>
+    <p>Der nächste Luzerner Solo- und Ensemblewettbewerb findet am 28./29. September 2024 in Marbach LU statt.</p>
+    <p />
+    <h2 class="text-secondary">TODO: Anmeldedaten</h2>
+    <ul>
+      <li>Vorname, Name</li>
+      <li>Adresse, Ort</li>
+      <li>Geburtsdatum (Datepicker)</li>
+      <li>Instrument (Dropdown: Cornet, Horn, Posaune, Tuba, Percussion)</li>
+      <li>Vortragswerk</li>
+    </ul>
   </article>
 </template>
